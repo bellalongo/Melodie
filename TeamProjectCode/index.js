9f670cc34a2627162cfef7e440dd6f5405d8377e
Alexander Brimhall <alexanderbrim38@gmail.com>
2022-11-13 17:53:09 -0700

    use this song button sorta works now

--- TeamProjectCode/index.js
+++ TeamProjectCode/index.js
@@ -56,6 +56,7 @@ app.use(
   })
 );
 
+app.use(express.static(__dirname + '/public'));
 // app.get('/', (req, res) =>{
 //   res.redirect('/login'); //this will call the /anotherRoute route in the API
 // });
