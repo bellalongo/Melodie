eea3551c82565389a045c30a12c40fbd38fa3072
Casey Rudzki <caru2171@colorado.edu>
2022-11-28 12:55:03 -0700

    modified spotify login permissions so can access top tracks

--- TeamProjectCode/index.js
+++ TeamProjectCode/index.js
@@ -215,7 +215,7 @@ app.get('/login_spotify', function(req, res) {
   res.cookie(stateKey, state);
  
   // your application requests authorization
-  var scope = 'streaming user-read-private user-read-email';
+  var scope = 'streaming user-read-private user-read-email user-library-read';
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
