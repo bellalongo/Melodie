bef7c31417702affd71cd2e92a30a91cc8cca0e1
Bella Longo <bellalongo.mail@gmail.com>
2022-11-30 20:36:45 -0700

    redirecting to home

--- TeamProjectCode/index.js
+++ TeamProjectCode/index.js
@@ -324,11 +324,7 @@ app.get('/callback', function(req, res) {
         });
  
         // we can also pass the token to the browser to make requests from there
-        res.redirect('/#' +
-          querystring.stringify({
-            access_token: access_token,
-            refresh_token: refresh_token
-          }));
+        res.redirect('/home');
           console.log(querystring.stringify({
             access_token: access_token,
             refresh_token: refresh_token
